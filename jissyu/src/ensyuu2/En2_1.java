f055b9f251621eab2b261b7accb0104e7e8a2b6e
ddt-jissyusei_shida <ddt.jissyusei1@gmail.com>
2017-02-07 23:33:38 +0900

    ensyuu2ブランチ作成　Add：En2-1

---
+++ jissyu/src/ensyuu2/En2_1.java
@@ -0,0 +1,20 @@
+package ensyuu2;
+
+public class En2_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//List2-6の2の箇所を、小数部をもつ実数値をxとyに代入するように変更して、その結果を考察せよ。
+		double x = 3.5;		//double型変数xに、3.5を代入
+		double y = 6.2;		//double型変数yに、6.2を代入
+
+		System.out.println("xの値は" + x + "です。");		//xの値を表示
+		System.out.println("yの値は" + y + "です。");		//yの値を表示
+
+		System.out.println("合計は" + (x+y) + "です。");		//xとyの合計を表示
+		System.out.println("平均は" + (x+y)/2 + "です。");		//xとyの平均を表示
+
+	}
+
+}

6b3373b4a13e2f4de481353ba2fe4b917b01bb7d
Shida <yasouki@jcom.home.ne.jp>
2017-02-07 23:07:44 +0900

    Add：ensyuuブランチ作成　

---
+++ jissyu/src/ensyuu1/En1_1.java
@@ -0,0 +1,12 @@
+package ensyuu1;
+
+public class En1_1 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//プログラム中の文の終端セミコロンをが欠如しているとどうなるか。プログラムをコンパイルして検証せよ。
+		System.out.println("HelloWorld!")
+	}
+
+}

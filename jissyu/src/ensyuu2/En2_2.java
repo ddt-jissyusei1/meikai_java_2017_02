cf791067cc69cd79122b68a1bb8dca159ed9e83a
ddt-jissyusei_shida <ddt.jissyusei1@gmail.com>
2017-02-07 23:48:48 +0900

    Add：演習２－２

---
+++ jissyu/src/ensyuu2/En2_2.java
@@ -0,0 +1,20 @@
+package ensyuu2;
+
+public class En2_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//三つのint型変数に値を代入し、合計と平均を求めるプログラムを作成せよ。
+
+		int x = 33;		//xに33を代入
+		int y = 87;		//yに87を代入
+		int z = 40;		//zに40を代入
+
+		System.out.println("xとyとzの合計は" + (x+y+z) + "です。");			//xyzの合計を表示
+		System.out.println("xとyとzの平均は" + (x+y+z)/3 + "です。");		//xyzの平均を表示
+
+
+	}
+
+}

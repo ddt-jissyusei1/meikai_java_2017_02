c6279675e002243a87f1093777acf9b86dbc4c6e
ddt-jissyusei_shida <ddt.jissyusei1@gmail.com>
2017-02-07 23:19:51 +0900

    Add：演習１－３

---
+++ jissyu/src/ensyuu1/En1_3.java
@@ -0,0 +1,14 @@
+package ensyuu1;
+
+public class En1_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//各行に1文字ずつ名前を表示するプログラムを作成せよ。
+		//姓と名のあいだは1行あけること。
+		System.out.println("志\n田\n\nゆ\nみ\nこ");
+
+	}
+
+}

c24b1873caa5de8928b3e991a520f519e5947b80
ddt-jissyusei_shida <ddt.jissyusei1@gmail.com>
2017-02-07 23:15:51 +0900

    Add：演習１－２

---
+++ jissyu/src/ensyuu1/En1_2.java
@@ -0,0 +1,13 @@
+package ensyuu1;
+
+public class En1_2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		//各行に1文字ずつ名前を表示するプログラムを作成せよ
+		System.out.println("志\n田\nゆ\nみ\n子");
+
+	}
+
+}

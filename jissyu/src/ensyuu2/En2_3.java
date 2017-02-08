617d39a21b84c616536e5abc3d367b0f574eea37
ddt-jissyusei_shida <ddt.jissyusei1@gmail.com>
2017-02-08 19:08:08 +0900

    Add：演習２－３

---
+++ jissyu/src/ensyuu2/En2_3.java
@@ -0,0 +1,20 @@
+package ensyuu2;
+
+import java.util.Scanner;
+
+public class En2_3 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		//キーボードから読み込んだ整数値をそのまま反復して表示するプログラムを作成せよ。
+		Scanner std = new Scanner(System.in);
+
+		System.out.print("整数値：");
+		int x = std.nextInt();
+
+		System.out.println(x + "と入力しましたね。");
+
+
+	}
+
+}

5d044ceb29daac3c3b4906bcdebccc6c50489306
ddt-jissyusei_shida <ddt.jissyusei1@gmail.com>
2017-02-08 20:00:42 +0900

    Add：演習２－７

---
+++ jissyu/src/ensyuu2/En2_7.java
@@ -0,0 +1,25 @@
+package ensyuu2;
+import java.util.Random;
+
+public class En2_7 {
+
+	public static void main(String[] args) {
+		/*以下に示すプログラムを作成せよ。
+		 * ・1桁の正の整数値（すなわち1以上9以下の値）をランダムに生成して表示。
+		 * ・1桁の負の整数値（すなわち‐9以上‐1以下の値）をランダム生成して表示。
+		 * ・2桁の正の整数値（すなわち10以上99以下の値）をランダム生成して表示。
+		 */
+
+		Random rand = new Random();
+
+		int sei = rand.nextInt(9)+1;			//正の整数のランダム値を生成して変数seiに代入
+		int fu = rand.nextInt(10)-10;				//負の整数のランダム値を生成して変数fuに代入
+		int futaketa = rand.nextInt(90)+10;	//二桁の整数のランダム値を生成して変数futaketaに代入
+
+		System.out.println("正の整数のランダム値は"+ sei +"です。");		//変数seiの値を表示
+		System.out.println("負の整数のランダム値は" + fu + "です。");		//変数fuの値を表示
+		System.out.println("二桁の整数のランダム値は" + futaketa + "です。");		//変数futaketaの値を表示
+
+	}
+
+}

0bc60faf93be00c9bd1b75939798267b70abd0c1
ddt-jissyusei_shida <ddt.jissyusei1@gmail.com>
2017-02-10 19:03:20 +0900

    Add：演習２－９

--- jissyu/src/ensyuu2/En2_9.java
+++ jissyu/src/ensyuu2/En2_9.java
@@ -9,15 +9,17 @@ import java.util.Random;
 public class En2_9 {
 
 	public static void main(String[] args) {
-		Random dubll = new Random();		//乱数生成用の変数
+		Random dubll = new Random();		//Randomクラスのインスタンス生成
 
 		double x = dubll.nextDouble();			//0.0以上1.0未満の実数値の乱数を生成して変数xに代入
-		double y = dubll.nextDouble()+10;			//0.0以上10.0未満の実数値の乱数を生成して変数yに代入
-		double z = dubll.nextDouble()-10;			//-1.0以上1.0未満の実数値の乱数を生成して変数yに代入
+		double y = dubll.nextDouble()+9;			//0.0以上10.0未満の実数値の乱数を生成して変数yに代入
+		double z = dubll.nextDouble()-1;			//-1.0以上1.0未満の実数値の乱数を生成して変数yに代入
 
 
 
 		System.out.println("0.0以上1.0未満の乱数は"+ x + "です。");		//変数xの値を表示
+		System.out.println("0.0以上10.0未満の乱数は" + y + "です。");	//変数yの値を表示
+		System.out.println("-1.0以上1.0未満の乱数は" + z + "です。");	//変数xの値を表示
 
 	}
 

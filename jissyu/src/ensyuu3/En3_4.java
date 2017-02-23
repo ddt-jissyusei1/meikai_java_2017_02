dfc2d264031100d3900bc3e174fe4acacab0e3ff
ddt-jissyusei_shida <ddt.jissyusei1@gmail.com>
2017-02-23 21:36:20 +0900

    Modify：演習３－４のコメント修正

--- jissyu/src/ensyuu3/En3_4.java
+++ jissyu/src/ensyuu3/En3_4.java
@@ -1,29 +1,43 @@
 package ensyuu3;
 import java.util.Scanner;
 
-/*二つの変数a,bに値を読み込んで、その大小関係を以下のいずれかで表示するプログラムを作成せよ。
- * 『aのほうが大きいです。』『bのほうが大きいです。』『aとbは同じ値です。』
+/*二つの変数number1,number2に値を読み込んで、その大小関係を以下のいずれかで表示するプログラムを作成せよ。
+ * 『number1のほうが大きいです。』『number2のほうが大きいです。』『number1とnumber2は同じ値です。』
  */
 
 public class En3_4 {
 
 	public static void main(String[] args) {
-		Scanner std = new Scanner(System.in);					//キーボードからの読み込みを行うプログラム
 
-		System.out.print("整数a：");						//整数aの値の入力を促す
-		int a = std.nextInt();								//入力された値を変数aに代入
+		//キーボードからの読み込みを行うプログラム
+		Scanner std = new Scanner(System.in);
 
-		System.out.print("整数b：");						//整数bの値の入力を促す
-		int b = std.nextInt();								//入力された値を変数bに代入
+		//数値の大小を比べるための一つ目の数字の入力を促す
+		System.out.print("整数１：");
+		//入力された値を変数number1に代入し、保持
+		int number1 = std.nextInt();
 
-		if(a > b){											//aの値がbより大きい
-			System.out.println("aのほうが大きいです。");	//その場合に出力する文
+		//二つ目の数字の入力を促す
+		System.out.print("整数２：");
+		//入力された値を変数number2に代入し、保持
+		int number2 = std.nextInt();
 
-		}else if(b > a){									//bの値がaより大きい
-			System.out.println("bのほうが大きいです。");	//その場合に出力する文
 
-		}else if(a == b){									//aとbの値が同じ
-			System.out.println("aとbは同じ値です。");		//その場合に出力する文
+
+		//number1の値がnumber2より大きいという条件
+		if(number1 > number2){
+			//number1のほうが大きいという判定文を出力する
+			System.out.println("整数１のほうが大きいです。");
+
+		//number2の値がnumber1より大きいという条件
+		}else if(number2 > number1){
+			//number2のほうが大きいという判定文を出力する。
+			System.out.println("整数２のほうが大きいです。");
+
+		//number1とnumber2の値が等しいという条件
+		}else if(number1 == number2){
+			//二つの値は等しいという判定文を出力する
+			System.out.println("整数１と整数２は同じ値です。");
 		}
 
 	}

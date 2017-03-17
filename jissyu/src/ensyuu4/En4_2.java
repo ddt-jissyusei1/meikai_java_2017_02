e1d031a22562dbdc348e9bb3e433e5e12fbc7bea
DDT_STAFF <ddt.jissyusei1@gmail.com>
2017-03-17 20:23:58 +0900

    プログラム的だったコメントを修正；変数名をわかりやすい名前に修正

--- jissyu/src/ensyuu4/En4_2.java
+++ jissyu/src/ensyuu4/En4_2.java
@@ -14,34 +14,33 @@ public class En4_2 {
 		//乱数を生成するためのプログラム
 		Random random = new Random();
 
-		int answer = random.nextInt(90)+10;	//当てるための10～99の生成した乱数を変数answerに代入して保持する
+		int correctNumber = random.nextInt(90)+10;	//当てるための10～99の生成した乱数を変数answerに代入して保持する
 
 		//ゲーム開始の文を出力して、ゲームスタートする
 		System.out.println("数当てゲーム開始！\n10～99の数字を当ててください。");
 
-		int number;							//プレイヤーが入力した回答を代入するための変数numberを宣言する
+		int playersAnswer;							//プレイヤーが入力した回答を代入するための変数numberを宣言する
 
 		//do～while文で正解までの工程を繰り返し実行する
 		do {
 			//プレイヤーに回答を入力してもらう
 			System.out.print("いくつかな？：");
-			//入力された数値を変数numberに代入して保持する
-			number = inputStream.nextInt();
+			//乱数を当てるために、入力された数値を変数numberに代入して保持する
+			playersAnswer = inputStream.nextInt();
 
-			//入力された値が、生成された乱数と同じであるか判定する
-			//正解より大きいまたは小さい場合、ヒント文を出力する
-			if(number > answer){
-				//正解の値より入力された値が大きい場合、もっと小さいというヒントを出す
+			//入力された値が正解より大きい場合に応じたヒントを出すコード
+			if(playersAnswer > correctNumber){
+				//正解の値より入力された値が大きい場合、もっと小さいというヒントを教える
 				System.out.println("もっと小さな数です。");
 
-				//他の条件の場合（入力された値が正解より小さいとき）、大きいというヒントを出す
-			}else if (number < answer){
-				//正解の値より入力された値が小さいというヒントを出す
+				//他の条件の場合（入力された値が正解より小さいとき）に応じたヒントを出すコード
+			}else if (playersAnswer < correctNumber){
+				//正解の値より入力された値が小さいというヒントを教える
 				System.out.println("もっと大きな数です。");
 			}
 
 			//入力された値と生成された乱数の値が等しくない（つまり不正解）の間、上記回答の入力工程を繰り返す
-		}while( number != answer);
+		}while( playersAnswer != correctNumber);
 
 
 		//入力された値と正解の値が等しかった場合（つまり正解）、doのループ文を抜け正解したと教える

2c7cf7d7adadd8660ddc9d5b14308138a60fc1d6
ddt-jissyusei_shida <ddt-jissyusei1@gmail.com>
2017-04-18 19:56:54 +0900

    演習４－２４、14行目　指摘のあった定数の命名を修正

--- jissyu/src/ensyuu4/En4_24.java
+++ jissyu/src/ensyuu4/En4_24.java
@@ -7,20 +7,20 @@ import java.util.Scanner;
  */
 public class En4_24 {
 	//プログラムの説明のための定数
-	private static final String PRIMENUMBER_PROGRAM_MESSAGE = "入力された値が素数であるか判定をします";
+	private static final String PRIME_NUMBER_PROGRAM_MESSAGE = "入力された値が素数であるか判定をします";
 	//入力を促す文のための定数
 	private static final String INPUT_NUMBER_MESSAGE = "素数判定をする数字を入力してください：";
 	//素数であるという判定文のための定数
-	private static final String IS_PRIMENUMBER_MESSAGE = "それは素数です。";
+	private static final String PRIME_NUMBER_MESSAGE = "それは素数です。";
 	//素数ではないという判定文のための定数
-	private static final String NOT_PRIMENUMBER_MESSAGE = "それは素数ではありません。";
+	private static final String NOT_PRIME_NUMBER_MESSAGE = "それは素数ではありません。";
 
 	public static void main(String[] args) {
 		//キーボードからの読み込みを行うためのプログラム
 		Scanner inputStream = new Scanner(System.in);
 
 		//プログラムの説明を出力する
-		System.out.println(PRIMENUMBER_PROGRAM_MESSAGE);
+		System.out.println(PRIME_NUMBER_PROGRAM_MESSAGE);
 
 		int primeNumber = 0;			//入力された値を代入するための変数を用意し、0で初期化する
 
@@ -46,11 +46,11 @@ public class En4_24 {
 			//numberToDivideとprimeNumberが等しい場合（割り切れず最後のループになった場合）
 			if(numberToDivide == primeNumber){
 				//素数は入力された値自身でしか割り切れないため、素数であるという判定文を出力する
-				System.out.println(IS_PRIMENUMBER_MESSAGE);
+				System.out.println(PRIME_NUMBER_MESSAGE);
 			//divideLeftNumber（剰余の値）が0と等しい場合、素数ではないという判定文を出力する
 			}else if(divideLeftNumber == 0){
 				//素数ではないという判定文を出力する
-				System.out.println(NOT_PRIMENUMBER_MESSAGE);
+				System.out.println(NOT_PRIME_NUMBER_MESSAGE);
 				//ひとつでも割り切れた時点で素数ではないと判明するので、判定のループを終了するためbreakで抜ける
 				break;
 			}

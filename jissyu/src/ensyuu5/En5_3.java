f16da556372a88a9a77c65e82dcd7a56ccb3df9b
ddt-jissyusei_shida <ddt-jissyusei1@gmail.com>
2017-04-25 20:27:37 +0900

    演習５－３追加

---
+++ jissyu/src/ensyuu5/En5_3.java
@@ -0,0 +1,23 @@
+package ensyuu5;
+
+/*
+ * 論理型の変数にtrueやfalseを代入して、その値を表示するプログラムを作成せよ。
+ */
+public class En5_3 {
+
+	public static void main(String[] args) {
+
+		//論理型の値の表示を確認するためにtrueを代入する
+		boolean judgeTrue = true;
+		//論理型の値の表示を確認するためにfalseを代入する
+		boolean judgeFalse = false;
+
+		//judgeTrueの値を出力する
+		System.out.println("変数judgeTrueの値は、" + judgeTrue);
+		//judgFalseの値を出力する
+		System.out.println("変数judgeFalseの値は、" + judgeFalse);
+
+
+	}
+
+}

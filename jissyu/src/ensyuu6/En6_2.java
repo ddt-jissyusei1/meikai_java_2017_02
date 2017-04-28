c849aa3a28f581a21b782f0d739bf5c3bdb7ad75
ddt-jissyusei_shida <ddt-jissyusei1@gmail.com>
2017-04-28 20:34:39 +0900

    演習６－２　16行目の説明的だったコメントを修正、14行目の配列の要素数を定数に修正

--- jissyu/src/ensyuu6/En6_2.java
+++ jissyu/src/ensyuu6/En6_2.java
@@ -6,15 +6,16 @@ package ensyuu6;
 public class En6_2 {
 	//プログラムの説明文のための定数
 	private static final String PROGRAM_MESSAGE = "int型の要素で配列が5の要素の値を、5から降順で表示します。";
-
+	//配列の要素数のための定数
+	private static final int ARRAY_NUMBER = 5;
 	public static void main(String[] args) {
 		//プログラムの説明文を出力する
 		System.out.println(PROGRAM_MESSAGE);
 
-		int[] arrayInt = new int[5];		//int型の要素数が5の配列を宣言する
+		int[] arrayInt = new int[ARRAY_NUMBER];		//int型の要素数が5の配列を宣言する
 		int arrayNumber = 5;				//配列の要素に代入するための値の変数を用意し、5で初期化する
 
-		//繰り返し処理でarrayNumberを1づつデクリメントした値を配列に代入し、出力する
+		//配列の値を順に出力するために、繰り返し処理でarrayNumberを1づつデクリメントした値を代入し、出力する
 		for(int loop = 0; loop < arrayInt.length; loop++){
 			//arrayIntにarrayNumberの値を代入する
 			arrayInt[loop] = arrayNumber;

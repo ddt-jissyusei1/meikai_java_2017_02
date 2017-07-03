963d183365c124ad76016260cf063f4db1dc9c8d
ddt-jissyusei_shida <ddt-jissyusei1@gmail.com>
2017-07-03 23:38:45 +0900

    演習６－１４　コミット漏れしていた修正（インデックスの位置修正）をコミット

--- jissyu/src/ensyuu6/En6_14.java
+++ jissyu/src/ensyuu6/En6_14.java
@@ -66,8 +66,8 @@ public class En6_14 {
 				answerMonth = inputAnswerStream.next();
 				//不正解だった場合の条件式
 				if(!answerMonth.equals(englishMonthString[month-1])){
-				 //不正解だった場合出力文
-				 System.out.println(WRONG_ANSWER_MESSAGE);
+					//不正解だった場合出力文
+					System.out.println(WRONG_ANSWER_MESSAGE);
 				}
 			//入力された値が正解の値と等しくない間出題を繰り返すための条件文
 			}while(!answerMonth.equals(englishMonthString[month-1]));

74293dac1bd2103158fb3705c0ef1759169f8a53
ddt-jissyusei_shida <ddt-jissyusei1@gmail.com>
2017-09-22 20:56:40 +0900

    演習9－4　Humanクラスのstaticを非staticに修正

--- jissyu/src/ensyuu9/Human2.java
+++ jissyu/src/ensyuu9/Human2.java
@@ -4,46 +4,46 @@ package ensyuu9;
  * Humanクラスに誕生日のフィールドとtoStringメソッドを追加したクラス
  */
 public class Human2 {
-	private static String name;								//名前のための変数
-	private static double height;							//身長のための変数
-	private static double weight;							//体重のための変数
-	private static int age;									//年齢のための変数
-	private static String bloodType;							//血液型のための変数
-	private static int humanBirthdayYear;					//誕生年のための変数
-	private static int humanBirthdayMonth;					//誕生月のための変数
-	private static int humanBirthdayDay;					//誕生日のための変数
-	private static Human2 birthday;							//誕生年月日のための変数
+	private String name;									//名前のための変数
+	private  double height;								//身長のための変数
+	private  double weight;								//体重のための変数
+	private  int age;										//年齢のための変数
+	private  String bloodType;								//血液型のための変数
+	private  int humanBirthdayYear;						//誕生年のための変数
+	private  int humanBirthdayMonth;						//誕生月のための変数
+	private  int humanBirthdayDay;							//誕生日のための変数
+	private  Human2 birthday;								//誕生年月日のための変数
 
 	//人間クラスのためのコンストラクタ
 	Human2(String name, double humanHeight, double humanWeight, int humanAge, String bloodType) {
-		Human2.name = name;										//名前のフィールドに仮引数nameの値を代入するための代入式
-		Human2.height = humanHeight;							//身長のフィールドに仮引数heightの値を代入するための代入式
-		Human2.weight = humanWeight;							//体重のフィールドに仮引数weightの値を代入するための代入式
-		Human2.age = humanAge;									//年齢のフィールドに仮引数ageの値を代入するための代入式
-		Human2.bloodType = bloodType;							//血液型のフィールドに仮引数bloodTypeの値を代入するための代入式
+		this.name = name;									//名前のフィールドに仮引数nameの値を代入するための代入式
+		this.height = humanHeight;							//身長のフィールドに仮引数heightの値を代入するための代入式
+		this.weight = humanWeight;							//体重のフィールドに仮引数weightの値を代入するための代入式
+		this.age = humanAge;								//年齢のフィールドに仮引数ageの値を代入するための代入式
+		this.bloodType = bloodType;							//血液型のフィールドに仮引数bloodTypeの値を代入するための代入式
 	}
 
 	//人間クラスの誕生年のためのコンストラクタ
 	Human2(int birthdayYear){
-		Human2.humanBirthdayYear = birthdayYear;				//誕生年のフィールドに仮引数birthdayYearの値を代入するための代入式
+		this.humanBirthdayYear = birthdayYear;				//誕生年のフィールドに仮引数birthdayYearの値を代入するための代入式
 	}
 
 	//人間クラスの誕生月のためのコンストラクタ
 	Human2(int birthdayYear, int birthdayMonth){
-		this(birthdayYear);										//同一処理を委ねるための同一クラス内のコンストラクタの呼び出し
-		Human2.humanBirthdayMonth = birthdayMonth;			//誕生月フィールドに仮引数birthdayMonthの値を代入するための代入式
+		this(birthdayYear);									//同一処理を委ねるための同一クラス内のコンストラクタの呼び出し
+		this.humanBirthdayMonth = birthdayMonth;			//誕生月フィールドに仮引数birthdayMonthの値を代入するための代入式
 	}
 
 	//人間クラスの誕生日のためのコンストラクタ
 	Human2(int birthdayYear, int birthdayMonth, int birthdayDay){
-		this(birthdayYear, birthdayMonth);						//同一処理を委ねるための同一クラス内のコンストラクタの呼び出し
-		Human2.humanBirthdayDay = birthdayDay;				//誕生日フィールドに仮引数birthdayDayの値を代入するための代入式
+		this(birthdayYear, birthdayMonth);					//同一処理を委ねるための同一クラス内のコンストラクタの呼び出し
+		this.humanBirthdayDay = birthdayDay;				//誕生日フィールドに仮引数birthdayDayの値を代入するための代入式
 	}
 
 	//人間クラスの誕生年月日のためのコンストラクタ
-	Human2(Human2 birthday){
+	Human2(Human2 human){
 		//同一処理を委ねるための同一クラス内のコンストラクタの呼び出し
-		this(Human2.humanBirthdayYear, Human2.humanBirthdayMonth, Human2.humanBirthdayDay);
+		this(human.humanBirthdayYear, human.humanBirthdayMonth, human.humanBirthdayDay);
 	}
 
 	//名前取得のためのメソッド
@@ -77,19 +77,19 @@ public class Human2 {
 	}
 
 	//誕生年を取得するためのメソッド
-	public static int getHumanBirthdayYear() {
+	public int getHumanBirthdayYear() {
 		//呼び出し元に誕生年を返却するためのreturn文
 		return humanBirthdayYear;
 	}
 
 	//誕生月を取得するためのメソッド
-	public static int getHumanBirthdayMonth() {
+	public int getHumanBirthdayMonth() {
 		//呼び出し元に誕生月を返却するためのreturn文
 		return humanBirthdayMonth;
 	}
 
 	//誕生日を取得するためのメソッド
-	public static int getHumanBirthdayDay() {
+	public  int getHumanBirthdayDay() {
 		//呼び出し元に誕生日を返却するためのreturn文
 		return humanBirthdayDay;
 	}
@@ -103,19 +103,19 @@ public class Human2 {
 	//誕生年の値を変更するためのセッタメソッド
 	public void setHumanBirthdayYear(int humanBirthdayYear) {
 		//受け取った値をフィールド変数へセットするための代入式
-		Human2.humanBirthdayYear = humanBirthdayYear;
+		this.humanBirthdayYear = humanBirthdayYear;
 	}
 
 	//誕生月の値を変更するためのセッタメソッド
 	public void setHumanBirthdayMonth(int humanBirthdayMonth) {
 		//受け取った値をフィールド変数へとセットするための代入式
-		Human2.humanBirthdayMonth = humanBirthdayMonth;
+		this.humanBirthdayMonth = humanBirthdayMonth;
 	}
 
 	//誕生日の値を変更するためのセッタメソッド
 	public void setHumanBirthdayDay(int humanBirthdayDay) {
 		//受け取った値をフィールド変数へとセットするための代入式
-		Human2.humanBirthdayDay = humanBirthdayDay;
+		this.humanBirthdayDay = humanBirthdayDay;
 	}
 
 	//人物のプロフィールを表示するためのメソッド
